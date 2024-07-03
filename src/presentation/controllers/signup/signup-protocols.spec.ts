304a90beb6196bf00ef0e99611ce331e52530c49
GustaVasconcelos <gustavo.vasconcelos.peixoto18@gmail.com>
2024-07-03 08:46:15 -0300

    test: add tests in signup protocols

---
+++ src/presentation/controllers/signup/signup-protocols.spec.ts
@@ -0,0 +1,7 @@
+import * as protocols from './signup-protocols'
+
+describe('Signup Protocols', () => {
+  it('should import protocols correctly', () => {
+    expect(protocols).toBeDefined()
+  })
+})
