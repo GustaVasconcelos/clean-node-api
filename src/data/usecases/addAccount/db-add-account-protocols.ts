499760b43f11850ec6ce79ee4a366be24b7ca14f
GustaVasconcelos <gustavo.vasconcelos.peixoto18@gmail.com>
2024-07-04 21:15:23 -0300

    refactor: group all protocols  in a single file

---
+++ src/data/usecases/addAccount/db-add-account-protocols.ts
@@ -0,0 +1,3 @@
+export { type AccountModel } from '../../../domain/models/account'
+export { type AddAccountModel, type AddAccount } from '../../../domain/usecases/add-account'
+export { type Encrypter } from '../../protocols/encrypter'
